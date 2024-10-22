4b43cf4ac19af8e1c6927d697d898e05f44f660e
Andrez18 <ieca.andresgomez@gmail.com>
2024-10-22 08:40:33 -0500

    cambio footer y configuración de tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+module.exports = {
+	content: [
+	  './src/**/*.{astro,html,js,jsx,svelte,vue}',
+	  './public/index.html'
+	],
 	theme: {
-		extend: {
-			colors: {
-				primary: '#371B70',
-				seconder:'#2D2187',
-				accent: '',
-				dark: '#131120'
-			}
-		},
+	  extend: {
+		colors: {
+		  primary: '#734191',
+		  complementario: '#91C141',
+		  backgroundDark: '#220B32',
+		  accent: '#F4D4CD',
+		  lightBackground: '#FAEFED',
+		  darkAccent: '#152012',
+		  secondary: '#C495E0',
+		  secondaryAccent: '#95E0C4'
+		}
+	  }
 	},
 	plugins: [],
-}
+  }
+  
